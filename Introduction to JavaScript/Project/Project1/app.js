21213086fcb0521c1bc78885a867dd3377d89839
joecole1011 <akawit_7@hotmail.com>
2021-02-17 23:20:57 +0700

    Sum js
    Sum js

---
+++ Introduction to JavaScript/Project/Project1/app.js
@@ -0,0 +1,59 @@
+let lotto_number = prompt("กรอกเลขที่ต้องการซื้้อ :")
+// console.log(lotto_number);
+// console.log(Math.floor(Math.random() * 100))
+document.getElementById("buy_lotto").innerHTML = lotto_number
+//ส่งค่า ไปที่ index.html 
+
+document.getElementById("result").innerHTML = (Math.floor(Math.random() * 100))
+//ส่งค่า ไปที่ index.html 
+
+
+//  var name = prompt("กรอกชื่ิอสิจ๊ะ")
+//  console.log("Hello " + name )
+
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// alert("Test")
+// var name = "Kim" 
+// console.log(name)
+
+// name = "Pop"     
+// console.log(name)
+
+// name = "Jame"     
+// console.log(name)
+
+// var firstname = "Akawit"
+// var lastname = "Nasoke"
+// console.log(firstname)
+// console.log(lastname)
+// alert(firstname)
+// alert(lastname)
+// alert(typeof firstname) //ตรวจสอบประเภทตัวแปร
+
+// console.log(10+2)
+// console.log(10*2)
+// console.log(10-5)
+// console.log(10/5)
+
+// console.log("Hello " + " Kim")
+
+// var age = 12 + 10
+// console.log(age)
+
+// console.log("Hello" + "Kim")
+// var name = "Kim" + "Kim"
+// console.log(name);
+// var age = (4+2*2/2*2+4+1-5+19)+91
+// {
+//     var x = 20
+//     let y = 15 // ใช้งานได้ในปีกกาเท่านั้น
+//     alert(y) 
+// }
+// var x = 20
+// {
+//     var x = 100
+//     let y = 15
+// }
+// console.log(x)
