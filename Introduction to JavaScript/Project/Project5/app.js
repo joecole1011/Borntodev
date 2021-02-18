426ab5658648f9aa7ec35a0f8f887e2257d3c3d8
Akawit Nasoke (Kimzun) <akawit_7@hotmail.com>
2021-02-18 16:09:02 +0700

    Add
    Add

---
+++ Introduction to JavaScript/Project/Project5/app.js
@@ -0,0 +1,6 @@
+ for(var counter = 0; counter <50; counter++){
+
+    // console.log( "Hello") 
+       console.log( "Hello" + counter) 
+ }
+        console.log("Exit");
---
+++ Introduction to JavaScript/Project/Project6/app.js
@@ -0,0 +1,6 @@
+let qrt = prompt("ใส่จำนวนสินค้า")
+   for(var i = 0 ; i < qrt ; i++){
+      // console.log(i)
+         console.log(i+1)
+
+   }
