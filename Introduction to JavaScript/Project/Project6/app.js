2f6b79be38cf031c883c3c64e0e9eb50f0334cff
joecole1011 <akawit_7@hotmail.com>
2021-02-18 23:01:54 +0700

    add P7
    add P7

--- Introduction to JavaScript/Project/Project6/app.js
+++ Introduction to JavaScript/Project/Project6/app.js
@@ -1,6 +1,24 @@
 let qrt = prompt("ใส่จำนวนสินค้า")
-   for(var i = 0 ; i < qrt ; i++){
+let sum = 0
+   for(var i = 1 ; i <= qrt ; i++){
       // console.log(i)
-         console.log(i+1)
-
+         // console.log(i+1)
+        
+         let item_price = prompt("ราคาสินค้าชิ้นที่" + i)
+         sum = sum + parseInt(item_price)
+         // document.getElementById("price-list").innerHTML += item_price // เพิ่มบรรทัดลงไปเรื่อยๆ <br>
+         document.getElementById("price-list").innerHTML += "รายการสินค้าชิ้นที่ " + i + " : " + item_price + " บาท" + "<br>"
+         // document.getElementById("result").innerHTML = "ราคารวม " + sum + " บาท"
    }
+      document.getElementById("result").innerHTML = "ราคารวม " + sum + " บาท"
+
+
+   // let qrt = prompt("ใส่จำนวนสินค้า")
+   // for(var i = 0 ; i < qrt ; i++){
+   //    // console.log(i)
+   //       // console.log(i+1)
+        
+   //       let item_price = prompt("ราคาสินค้าชิ้นที่" + i)
+   //       document.getElementById("price-list").innerHTML += item_price
+
+   // }
---
+++ Introduction to JavaScript/Project/Project7/app.js
@@ -0,0 +1,51 @@
+let round = prompt ("คุณจะเล่นทั้งหมดกี่รอบ")
+let sum_round = 1
+ let sum_answer = 0
+for (var i = 1; i <= round ; i++) {
+      var answer = prompt("หัว หรือ ก้อย พิมพ์มาเลย !") 
+      var random_answer = ""
+         // Math.floor(Math.random * 10) สุ่มเลข แล้วปัดเศษออก
+      if (Math.floor(Math.random()*10) <= 4 ) {
+            //หัว
+            random_answer = "หัว"
+      } else {
+         //ก้อย
+            random_answer = "ก้อย"
+      }
+      if (answer == random_answer ) {
+         alert("ตอบถูก ! ยินดีด้วย")
+         sum_answer =  sum_answer + sum_round
+         // console.log(i);   
+         
+      } 
+      else { 
+         alert("ตอบผิด ! เสียใจด้วย")
+      }
+
+      document.getElementById("game-list").innerHTML += "ครั้งที่ " + i + " คุณตอบ " + answer + " : " + "คำตอบคือ " + random_answer + "<br>"
+      // console.log(random_answer+ " : " + answer);
+}
+      //รวมคะแนน
+      document.getElementById("result").innerHTML = "คะแนนรวม " + sum_answer + " คะแนน"
+
+
+// let round = prompt ("คุณจะเล่นทั้งหมดกี่รอบ")
+// for (var i = 0; i < round ; i++) {
+//       var answer = prompt("หัว หรือ ก้อย พิมพ์มาเลย !") 
+//       var random_answer = ""
+//          // Math.floor(Math.random * 10) สุ่มเลข แล้วปัดเศษออก
+//       if (Math.floor(Math.random()*10) <= 4 ) {
+//             //หัว
+//             random_answer = "หัว"
+//       } else {
+//          //ก้อย
+//             random_answer = "ก้อย"
+//       }
+//       if (answer == random_answer ) {
+//          alert("ตอบถูก ! ยินดีด้วย")
+//       } 
+//       else { 
+//          alert("ตอบผิด ! เสียใจด้วย")
+//       }
+//       console.log(random_answer+ " : " + answer);
+// }
