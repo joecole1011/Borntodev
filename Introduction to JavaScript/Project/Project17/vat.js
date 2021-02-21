b4ff5140b9b1873eebbe52dd3ec5d376a093221b
joecole1011 <akawit_7@hotmail.com>
2021-02-21 22:07:21 +0700

    add Vat
    add Vat

---
+++ Introduction to JavaScript/Project/Project17/vat.js
@@ -0,0 +1,22 @@
+    let qrt = prompt("ใส่จำนวนสินค้า")
+    let sum = 0
+    let disper = 0
+    let total = 0
+    for(var i = 1 ; i <= qrt ; i++){
+ 
+          let item_price = prompt("ราคาสินค้าชิ้นที่" + i)
+          sum = sum + parseInt(item_price)
+          // document.getElementById("price-list").innerHTML += item_price // เพิ่มบรรทัดลงไปเรื่อยๆ <br>
+          document.getElementById("price-list").innerHTML += "รายการสินค้าชิ้นที่ " + i + " : " + item_price + " บาท" + "<br>"
+          // document.getElementById("result").innerHTML = "ราคารวม " + sum + " บาท"
+    }
+       document.getElementById("result").innerHTML = "ราคารวม " + sum + " บาท"
+
+       let per = prompt("ใส่เปอร์เซ็นต์ส่วนลด")
+       document.getElementById("per").innerHTML = "ลดไป " + per + " เปอร์เซ็น"
+
+       disper = (sum * per) / 100
+       document.getElementById("per-dis").innerHTML = "ส่วนลดที่ได้ " + disper + " บาท"
+
+       total = sum - disper 
+       document.getElementById("totalPer").innerHTML = "ราคาเมื่อคิดส่วนลดแล้ว " + total + " บาท" 
